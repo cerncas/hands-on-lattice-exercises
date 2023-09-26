3b45dc65d7300bf531e7ca00d8a1790914250f07
Davide Gamba <davide.gamba@cern.ch>
2023-09-26 17:15:30 +0200

    re-composed all solutions, before eliminating old stuff

--- README.md
+++ README.md
@@ -7,9 +7,13 @@ This repository contains all material used during the the course.
 The repository is based on the material prepared and updated during previous courses by *G. Sterbini, A. Latina, A. Poyet,* CERN and *V. Ziemann,* Uppsala University.
 
 - [Setup_Instructions.md](./Setup_Instructions.md) contains all information to get ready for the course. We kindly ask the student to read this document **before coming** to CAS to **prepare yourself** (and **your laptop**) for the course. 
-- [CAS_Optics_Primer.pdf](./CAS_Optics_Primer.pdf) is the primer of the course. You are invited to consult it before, during, and after the course. 
-- [0*.ipynb](./01_Guided_Exercises.ipynb) will be the "canvas" used during the course. It contains all exercise with "incomplete" parts that the student will have to fill.
-- [Solutions.ipynb](./Solutions.ipynb) contains the solutions to all the exercises that we will tackle during the course. This notebook is provided for the student convenience, but it is clearly not expected to be used before and/or during the course itself.
+- [CAS_Optics_Primer.pdf](./CAS_Optics_Primer.pdf) is the primer of the course. You are invited to consult it before, during, and after the course.
+- The `*.ipynb` notebooks will be the "canvas" used during the course. They contain all exercise with "incomplete" parts that the student will have to fill in. Solutions to all the exercises are also provided in `*_Solutions.ipynb`. These notebooks are provided for the student convenience, but they are clearly not expected to be used before and/or during the course itself...
+    - [01_Guided_Exercises](./01_Guided_Exercises.ipynb) Guided/already solved exercises to introduce the hands-on course.
+    - [02_Single_Particle_Beamline](./02_Single_Particle_Beamline.ipynb) Exercises to probe single particle dynamics.
+    - [03_Multi_Particles_Beamline](./03_Multi_Particles_Beamline.ipynb) Exercises to probe multi-particle dynamics.
+    - [04_Periodic_Systems](./04_Periodic_Systems.ipynb) Exercises to probe the concept of peridic systems.
+    - [05_Advanced_Exercises](./05_Advanced_Exercises.ipynb) Advanced exercises, including matching, non-linear dynamics, ...
 
 > **NOTE:** the material in this repository is expected to evolve over time thanks to the feedback received from you! Please don't hesitate to transmit us your comments, suggestions, and complains!
 
@@ -47,7 +51,7 @@ A solution could be to use the [Print extension](https://marketplace.visualstudi
 
 ### During the course
 
-The students are expected to download [Exercises.ipynb](./Exercises.ipynb) on their computer, and open it using Jupyter Lab.
+The students are expected to download the [whole repository](https://github.com/cerncas/hands-on-lattice-exercises/archive/refs/heads/master.zip) on their computer, and open the various notebooks using Jupyter Lab.
 The presenter can also use Jupyter Lab and do the exercise with the students. To launch Jupyter Lab, move in a terminal to this folder and execute:
 
 ```bash
--- tracking_library.py
+++ tracking_library.py
@@ -1,3 +1,7 @@
+###################################################
+# Import of all necessary packages
+###################################################
+
 # numpy: our main numerical package
 import numpy as np
 # matplotlib and seaborn: our plotting packages
@@ -27,7 +31,7 @@ from matplotlib import pyplot as plt
 
 # Modelling of a drift
 def D(L):
-    '''Returns the list of a L-long drift'''
+    '''Returns a list containing a single "drift" of length L'''
     # NB: we return a list with a dict
     # the dict contains the matrix (the transformation)
     # and the element length 
@@ -36,7 +40,7 @@ def D(L):
 
 # Modelling of a quadrupole
 def Q(f):
-    '''Returns the list of a quadrupole with focal length f'''
+    '''Returns a list containing a quadrupole with focal length f'''
     # NB: we return a list with a dict
     # the dict contains the matrix (the transformation)
     # and the element length 
@@ -45,7 +49,7 @@ def Q(f):
 
 # From a list of elements - or beamline - to an equivalent single element
 def getEquivalentElement(beamline):
-    '''Returns the equivalent single element of a beamline'''
+    '''Returns the single element which is equivalent of the given beamline'''
     # we start from an identity matrix (np.eye)
     # with the same dimension of the matrix of the
     # first element of the beamline
@@ -57,26 +61,36 @@ def getEquivalentElement(beamline):
         equivalentElement = equivalentElement @ elements['matrix']
         # and we add the lengths
         length = length + elements['length']
-    # we return the dict with the "usual" keys (matrix and length)
-    # as for the definition of the D and Q functions
+    # we return the dict with the "usual" keys (matrix and length) embedded in a
+    #  list (with a single element), as for the definition of the D and Q functions
     return [{'matrix':equivalentElement, 'length':length}]
 
 
 # Tracking particles along a beamline
-def transportParticles(x0,beamline,s0=0):
-    '''Track the particle(s) x0 along the given beamline. 
-    If needed, one can specify an initial longitudinal position s0, otherwise set to 0.
+def transportParticles(X_0, beamline, s_0=0):
+    '''Track the particle(s) `X_0` along the given `beamline`. 
+    If needed, one can specify an initial longitudinal position `s_0`, otherwise set to 0.
+
+    It will return a dictionary containing the following key:values
+       'x': a NxM numpy array with the M-particles x position for all N-elements of the beamline
+       'xp': a NxM numpy array with the M-particles x' angles for all N-elements of the beamline
+       's': a N-long numpy array with the longitudinal position of the N-elements of the beamline
+       'coords': a Nx2xM numpy array with all M-particles coordinates (both x and x') at all N-elements of the beamline
+    
+    Disclaimer: if beamline is made of 5 elements, the output will have 5+1 "elements" as it will also 
+                return include the initial particle coordinates.
     '''
-    coords = [x0]
-    s = [s0]
-    for elements in beamline:
-        coords.append(elements['matrix'] @ coords[-1])
-        s.append(s[-1] + elements['length']) 
-    coords = np.array(coords).transpose()
-    return {'x':  coords[:,0,:], # [particle_idx, s_idx]
-            'px': coords[:,1,:], # [particle_idx, s_idx]
-            's':  np.array(s), # [s_idx]
-            'coords': coords,} # [particle_idx, coord_idx, s_idx]
+    coords = [X_0]
+    s = [s_0]
+    for element in beamline:
+        coords.append(element['matrix'] @ coords[-1])
+        s.append(s[-1] + element['length']) 
+    coords = np.array(coords)
+    s = np.array(s)
+    return {'x':  coords[:,0,:], # [s_idx, particle_idx]
+            'xp': coords[:,1,:], # [s_idx, particle_idx]
+            's':  s,   # [s_idx]
+            'coords': coords,}   # [s_idx, coord_idx, particle_idx]
 
 
 
@@ -119,54 +133,29 @@ def transportSigmas(sigma_0, beamline):
 ###################################################
 
 def twiss(beamline):
-    '''Returns the Q, and the Twiss parameters beta, alpha, gamma of the beamline'''
+    '''
+    Computes and returns the closed solution (if it exist!) Tune (Q) and Twiss parameters 
+    beta, alpha, gamma of the given beamline.
+    '''
+
+    # first, compute the equivalent "One-Turn-Map", and extract its matrix:
     OTM = getEquivalentElement(beamline)
     R = OTM[0]['matrix']
+    
+    # check that this matrix is stable:
+    if np.abs(0.5*(R[0,0]+R[1,1])) > 1:
+        raise ValueError('This beamline is not stable!')
+    
+    # all relevant Twiss parameters can be extrcted from the matrix:
     mu = np.arccos(0.5*(R[0,0]+R[1,1]))
     if (R[0,1]<0): 
-        mu = 2*np.pi-mu;
+        mu = 2*np.pi-mu
     Q = mu/(2*np.pi)
     beta = R[0,1]/np.sin(mu)
     alpha = (0.5*(R[0,0]-R[1,1]))/np.sin(mu)
     gamma = (1+alpha**2)/beta
+    
     return Q, beta, alpha, gamma
-
-
-def myOTM_trace(L, f):
-    fodo_lattice = 5*D(L/10) + Q(f) + 10*D(L/10) + Q(-f) + 5*D(L/10)
-    OTM = getEquivalentElement(fodo_lattice)
-    return np.trace(OTM[0]['matrix'])
-
-
-def particle_emittance(x, xp, beta, alpha):
-    '''Returns the single particle emittance for a given 
-       x, xp particle coordinates and assumed beta and alpha parameters
-    '''
-    # compute gamma
-    gamma = (alpha**2 + 1)/beta
-    # compute and return the associated single particle emittance
-    epsilon = gamma*x**2 + 2*alpha*x*xp + beta*xp**2
-    return epsilon
-
-
-def ellipse_points(emittance, beta, alpha, n_points = 100):
-    ''' Returns the x,x' coordinates of an ellipse in phase space for 
-        the given Twiss parameters (beta, gamma, emittance)
-    '''
-    # generate a uniform sampling of phases:
-    thetas = np.linspace(0, 2*np.pi, n_points)
-    # generate coordinates
-    x  = np.sqrt(emittance*beta)*np.cos(thetas)
-    xp = -np.sqrt(emittance/beta)*(alpha*np.cos(thetas)-np.sin(thetas))
-    # return them in our usual form
-    return np.array([x, xp])
-
-
-def penalty_function(f):
-    fodo_lattice = Q(f[0]) + 10*D(L_2/10) + Q(-f[1]) + 10*D(L_2/10)
-    fodo_lattice_compressed = getEquivalentElement(fodo_lattice)
-    M = fodo_lattice_compressed[0]['matrix']@sigma_60@(fodo_lattice_compressed[0]['matrix']).transpose() - sigma_90
-    return np.linalg.norm(M)
 
 
 
